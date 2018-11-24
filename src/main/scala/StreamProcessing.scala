e06c211928395d2ceee149a02ff7c91139726454
arsee <kuzmin.arsee@yandex.ru>
2018-11-24 21:11:11 +0300

    html parsing

--- src/main/scala/StreamProcessing.scala
+++ src/main/scala/StreamProcessing.scala
@@ -1,10 +1,12 @@
+import StreamProcessing.TwitterEntity
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import com.github.catalystcode.fortis.spark.streaming.rss.RSSInputDStream
+import com.github.catalystcode.fortis.spark.streaming.rss.{RSSEntry, RSSInputDStream}
 import com.github.catalystcode.fortis.spark.streaming.html.HTMLInputDStream
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -26,13 +28,40 @@ object StreamProcessing {
     stream.foreachRDD(rdd=>{
       val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
       import spark.sqlContext.implicits._
-      rdd.toDS().show()
+
+     rdd.toDS()
+      parseTweets(rdd.toDS())
+
     })
 
     // run forever
     ssc.start()
     ssc.awaitTermination()
   }
+  def parseTweets(ds: Dataset[RSSEntry]): String ={
+    var tweets:Array[TwitterEntity] = Array()
+    ds.foreach((x:RSSEntry)=>
+      {tweets= tweets ++(Array(new TwitterEntity(x.uri,x.description.value)))
+      println(x.description.value+"\n___")
+      })
+
+    ""
+  }
+  class TwitterEntity(var url:String,var content:String,var author:String) {
+    def this(uri:String,description: String){
+      this("","","")
+      val regex = "^>(.*)</a>$".r
+      var seqe = Seq(description)
+      val value = seqe.collect { case regex(a) => a.trim }
+      val  textRegex = "^</a>(.*)$".r
+      var text = seqe.collect { case textRegex(a) => a.trim }
+      print(value)
+      url = uri
+      author =value.head
+
+    }
+  }
+
 }
 
 
